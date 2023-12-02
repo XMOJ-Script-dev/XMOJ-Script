b5b1af8aed7ad871c5931b168d387e9a2af51483
boomzero <thomas_rainbowfish@icloud.com>
2023-12-02 12:36:22 +0800

    fix

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      1.0.252
+// @version      1.0.253
 // @description  XMOJ增强脚本
 // @author       @XMOJ-Script-dev, @langningchen and the community
 // @namespace    https://github/langningchen
