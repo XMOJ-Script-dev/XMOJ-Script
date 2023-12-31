5f20905afe3b9db1114d5e9eddc0fab1e5f261af
Shan Wenxiao <seanoj_noreply@yeah.net>
2023-12-31 18:05:45 +0800

    Update XMOJ.user.js
    Signed-off-by: Shan Wenxiao <seanoj_noreply@yeah.net>

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -17,6 +17,8 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setClipboard
 // @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @homepage     https://www.xmoj-bbs.tech/
 // @supportURL   https://github.com/XMOJ-Script-dev/XMOJ-Script
 // @connect      api.xmoj-bbs.tech
