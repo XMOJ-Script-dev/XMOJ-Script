080645742d2f3055351b62d42a33402d5e11f37b
Zhu Chenrui <85378277+boomzero@users.noreply.github.com>
2024-01-01 09:21:19 +0800

    Revert "增加权限"

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      1.1.12
+// @version      1.1.11
 // @description  XMOJ增强脚本
 // @author       @XMOJ-Script-dev, @langningchen and the community
 // @namespace    https://github/langningchen
@@ -17,8 +17,6 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setClipboard
 // @grant        unsafeWindow
-// @grant        GM_setValue
-// @grant        GM_getValue
 // @homepage     https://www.xmoj-bbs.tech/
 // @supportURL   https://github.com/XMOJ-Script-dev/XMOJ-Script
 // @connect      api.xmoj-bbs.tech
