324df6d7b3754c821db0872139fd7cab8e7ba8c6
Zhu Chenrui <85378277+boomzero@users.noreply.github.com>
2024-01-01 12:35:08 +0800

    Update AddonScript.js
    Signed-off-by: Zhu Chenrui <85378277+boomzero@users.noreply.github.com>

--- AddonScript.js
+++ AddonScript.js
@@ -3,7 +3,6 @@ if (localStorage.getItem("UserScript-2024newyear") == null) {
     let InputValue = prompt("小明的OJ增强脚本开发组祝您新年快乐！输入2024确认 \nPowered by xmoj-script version " + GM_info.script.version);
     if (InputValue != "2024") {
         alert("小明的OJ增强脚本开发组祝您新年快乐！");
-        window.location.href = "https://www.seanoj.edu.eu.org/#Install";
     }
     else {
         localStorage.setItem("UserScript-2024newyear", "true")
