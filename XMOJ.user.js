b5dbf83e37bbef67317df8574dde0cb49ec36f0c
Zhu Chenrui <85378277+boomzero@users.noreply.github.com>
2024-01-22 21:24:20 +0800

    fix spellcheck (#377)
    * spellcheck that works
    
    * 1.1.21
    
    * Update version info to 1.1.21
    
    * rel notes
    
    * Update time and description of 1.1.21
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      1.1.20
+// @version      1.1.21
 // @description  XMOJ增强脚本
 // @author       @XMOJ-Script-dev, @langningchen and the community
 // @namespace    https://github/langningchen
@@ -3667,7 +3667,7 @@ int main()
                             <label for="ToUser">接收用户</label>
                         </div>
                         <div class="col-md form-floating">
-                            <input class="form-control" id="Content" placeholder=" ">
+                            <input spellcheck="true" class="form-control" id="Content" placeholder=" ">
                             <label for="Content">内容</label>
                         </div>
                     </div>
@@ -3908,7 +3908,7 @@ int main()
                     </div>
                     <div class="form-group mb-3">
                         <label for="Title" class="mb-1">标题</label>
-                        <input type="text" class="form-control" id="TitleElement" placeholder="请输入标题">
+                        <input spellcheck="true" type="text" class="form-control" id="TitleElement" placeholder="请输入标题">
                     </div>
                     <div>
                         <label for="ContentElement" class="mb-1">回复</label>
