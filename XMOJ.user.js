1290f74ec0f672fd6c7a66b2e36e9d0734e25fab
boomzero <thomas_rainbowfish@icloud.com>
2024-08-06 22:12:55 +0800

    Error Reporting

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -4847,4 +4847,11 @@ int main()
     }
 }
 
-main();
+try {
+    main();
+} catch (e) {
+    console.error(e);
+    if (UtilityEnabled("DebugMode")) {
+        alert("Internal error!\n\n" + e + "\n\n" + "If you see this message, please report it to the developer.\n\n Don't want to see this message? Disable DebugModes.");
+    }
+}
