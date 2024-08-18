5d36e6d991940886d78c460afa3817f196dde4ed
zhouyiqing0304 <94766918+zhouyiqing0304@users.noreply.github.com>
2024-08-18 16:10:23 +0800

    Update XMOJ.user.js
    Signed-off-by: zhouyiqing0304 <94766918+zhouyiqing0304@users.noreply.github.com>

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -888,6 +888,7 @@ async function main() {
                         }
                         else if (document.querySelector("#navbar > ul.nav.navbar-nav.navbar-right > li > ul > li:nth-child(3) > a > span") != undefined && document.querySelector("#navbar > ul.nav.navbar-nav.navbar-right > li > ul > li:nth-child(3) > a > span").innerText != "个人中心") {
                             let PopupUL = document.querySelector("#navbar > ul.nav.navbar-nav.navbar-right > li > ul");
+                            PopupUL.style.cursor = 'pointer';
                             PopupUL.innerHTML = `<li class="dropdown-item">修改帐号</li>
                                              <li class="dropdown-item">个人中心</li>
                                              <li class="dropdown-item">短消息</li>
