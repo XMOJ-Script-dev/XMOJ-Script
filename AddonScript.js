629912948379e24562abf098c2cc62830b08051e
Zhu Chenrui <boomzero_zcr@outlook.com>
2024-12-03 16:30:35 +0800

    Update AddonScript.js
    Signed-off-by: Zhu Chenrui <boomzero_zcr@outlook.com>

--- AddonScript.js
+++ AddonScript.js
@@ -1,13 +1,1 @@
-        function getRandomColor() {
-            const letters = '0123456789ABCDEF';
-            let color = '#';
-            for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
-
-        setInterval(function() {
-            document.getElementById('colorText').style.color = getRandomColor();
-        }, 500);
 console.log("AddonScript.js has loaded.");
