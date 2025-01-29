0fdece17139412fe7d27d08fc44f74dd05387c2b
boomzero <thomas_rainbowfish@icloud.com>
2025-01-29 08:36:31 +0800

    test

--- updateNames.js
+++ updateNames.js
@@ -5,7 +5,7 @@ for (let i = 1000; i <= newestContest; i++) {
   let page = await fetch(url);
   let html = await page.text();
   let $ = cheerio.load(html);
-  let contestName = $('title').text().split(' - ')[0]; //cjh loves Kelly
+  let contestName = $('title').text().split(' - ')[0]; //test gpg
   let contest = await Contest.findOne({ contestId: i });
   contest.contestName = contestName;
   await contest.save();
