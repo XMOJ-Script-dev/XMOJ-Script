75271e06214aa23ab08a1dbcc0715db62b46e9d9
boomzero <thomas_rainbowfish@icloud.com>
2025-01-29 08:26:51 +0800

    test

---
+++ updateNames.js
@@ -0,0 +1,12 @@
+const cheerio = require('cheerio');
+let newestContest = 8218;
+for (let i = 1000; i <= newestContest; i++) {
+  let url = `https://www.xmoj.tech/contestrank.xls.php?cid=${i}`;
+  let page = await fetch(url);
+  let html = await page.text();
+  let $ = cheerio.load(html);
+  let contestName = $('title').text().split(' - ')[0]; //cjh loves Kelly
+  let contest = await Contest.findOne({ contestId: i });
+  contest.contestName = contestName;
+  await contest.save();
+}
