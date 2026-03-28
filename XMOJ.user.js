0698aaa8c81633c835012e66e09f542f9767a354
Shan Wenxiao <seanoj_noreply@yeah.net>
2026-03-28 09:09:08 +0800

    Revert "Update to release 3.4.0"

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      3.4.0
+// @version      3.3.5
 // @description  XMOJ增强脚本
 // @author       @XMOJ-Script-dev, @langningchen and the community
 // @namespace    https://github/langningchen
