f64d95cabeb389d1ab558402456237b2b2e24e9e
Zhu Chenrui <boomzero_zcr@outlook.com>
2026-08-21 12:30:53 +0800

    refactor: build submission links safely

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -3502,11 +3502,19 @@ async function main() {
                             SolutionIDs.push(SID);
                             if (UtilityEnabled("ResetType")) {
                                 Temp[i].childNodes[0].remove();
-                                let resubmitLink = Temp[i].childNodes[6].children[1];
-                                Temp[i].childNodes[0].innerHTML = "<a href=\"https://www.xmoj.tech/showsource.php?id=" + SID + "\">" + SID + "</a>";
+                                let resubmitLink = Temp[i].childNodes[6]?.children[1];
+                                let sourceCell = Temp[i].childNodes[0];
+                                let sourceLink = document.createElement("a");
+                                sourceLink.href = "https://www.xmoj.tech/showsource.php?id=" + SID;
+                                sourceLink.innerText = SID;
+                                sourceCell.replaceChildren(sourceLink);
                                 // Submissions with PID 0 do not have a resubmit link.
                                 if (resubmitLink != null) {
-                                    Temp[i].childNodes[0].innerHTML += " <a href=\"" + resubmitLink.href + "\">重交</a>";
+                                    let newResubmitLink = document.createElement("a");
+                                    newResubmitLink.href = resubmitLink.href;
+                                    newResubmitLink.innerText = "重交";
+                                    sourceCell.appendChild(document.createTextNode(" "));
+                                    sourceCell.appendChild(newResubmitLink);
                                 }
                                 Temp[i].childNodes[1].remove();
                                 Temp[i].childNodes[1].children[0].removeAttribute("class");
